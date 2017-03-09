7db42d20a50726fab359fb8392d36558a6276d9e
Carsten Dormann <carsten.dormann@biom.uni-freiburg.de>
2017-03-09 12:23:37 +0100

    updated package

--- FReibier_packaging.R
+++ FReibier_packaging.R
@@ -3,6 +3,6 @@ setwd("~/Data/aktuell/Misc/R-stuff/FReibier")
 document() # process R-functions into .RD files, change namespace
 setwd("..")
 # build the .tar.gz file:
-build("FReibier")  
+build("FReibier", path="FReibier")  
 # install on the computer:
 install("FReibier")
